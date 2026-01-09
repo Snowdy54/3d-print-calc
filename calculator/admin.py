39bf03506483f554837d689dbd77485723729e47
Andrey Milchakov <predator.alien.2017@mail.ru>
2026-01-09 23:13:56 +0500

    Implement cost calculation in PrintOrder model

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Printer, Filament, PrintOrder
 
-# Register your models here.
+@admin.register(Printer)
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'power_consumption', 'purchase_price', 'owner')
+
+@admin.register(Filament)
+class FilamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_per_spool', 'weight_g', 'owner')
+
+@admin.register(PrintOrder)
+class PrintOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'printer', 'filament', 'model_weight_g', 'total_cost', 'created_at')
--- calculator/models.py
+++ calculator/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from decimal import Decimal
 
 class Printer(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название принтера")
@@ -29,3 +30,15 @@ class PrintOrder(models.Model):
 
     def __str__(self):
         return f"Заказ {self.id} — {self.printer.name}"
+    
+    def save(self, *args, **kwargs):
+        amortization = (self.printer.purchase_price / 2000) * Decimal(str(self.print_time_hours))
+        
+        electricity = (Decimal(str(self.printer.power_consumption)) / 1000) * Decimal(str(self.print_time_hours)) * Decimal('6.0')
+        
+        plastic = (self.filament.price_per_spool / self.filament.weight_g) * self.model_weight_g
+        
+        self.total_cost = amortization + electricity + plastic
+        
+        # Сохраняем результат в базу
+        super().save(*args, **kwargs)
